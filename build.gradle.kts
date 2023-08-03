942b0ba2bc58787e9ac7f4ef42a3a4f9b5962b43
Mars <49356012+7003Mars@users.noreply.github.com>
2023-08-03 18:55:19 +0800

    jitpain x2

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ dependencies {
 mindustry {
 
     dependency {
-        mindustry on "v145.1"
-        arc on "v145"
+        mindustry on "0e31103"
+        arc on "v145.1"
     }
     client{
         mindustry from GameLocation("mindustry-antigrief", "mindustry-client-v7-builds",
